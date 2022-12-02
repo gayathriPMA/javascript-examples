5a98146b8cc03f9d37273186d7ab68b2cb85d11a
gayathriPMA <gayathri@pickmyad.com>
2022-12-02 19:01:25 +0530

    26TH PROGRAM

--- intro25.js
+++ intro25.js
@@ -1,12 +1,15 @@
 // program to sort words in alphabetical order
 //take input
-const s= PROMPT('here I am')
+const s= 'here I am'
 //create array
 const words=s.split(' ')
 console.log(words);
 //sort elements
 words.sort()
+words.join()
+
 console.log("The Alphabetical order is");
 for (const result of words){
     console.log(result);
 }
+//words.join()
